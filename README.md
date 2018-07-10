c53c3c219bd0604ee6aa413ae47f79132c26532c
Unknown <frederickmorin18@gmail.com>
2018-07-10 16:21:26 -0400

    Implement sorting using react-data-sort

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 Using Semantic UI. See [Semantic UI React Introduction](https://react.semantic-ui.com/introduction) for details.
 
+Using [react-data-sort](https://www.npmjs.com/package/react-data-sort) render prop pattern component for sorting alphabetically by title or numerically by id.
+
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
--- src/components/Routes/Content/ContentSegment.js
+++ src/components/Routes/Content/ContentSegment.js
@@ -1,21 +1,61 @@
 import React from 'react';
-import { Card, Header, Loader, Segment } from 'semantic-ui-react';
+import Datasort from 'react-data-sort';
+import { Card, Header, Icon, Loader, Menu, Segment } from 'semantic-ui-react';
 
 class ContentSegment extends React.Component {
-  state = {};
+  state = {
+    sortBy: 'id',
+    direction: 'asc',
+  };
+
+  handleSort = newSortBy => {
+    this.setState(prevState => {
+      let { direction, sortBy } = prevState;
+      if (sortBy === newSortBy) {
+        direction = direction === 'asc' ? 'desc' : 'asc';
+      } else {
+        sortBy = newSortBy;
+      }
+
+      return { direction, sortBy };
+    });
+  };
+
   render() {
+    const { sortBy, direction } = this.state;
     const { contentType, loading, content, users } = this.props;
     return (
       <React.Fragment>
-        <Header
-          as="h3"
-          color="teal"
-          textAlign="left"
-          attached="top"
-          style={{ textTransform: 'capitalize' }}
-        >
-          {contentType}
-        </Header>
+        <Menu attached="top" borderless>
+          <Menu.Item>
+            <Header
+              as="h3"
+              color="teal"
+              textAlign="left"
+              style={{ textTransform: 'capitalize' }}
+            >
+              {contentType}
+            </Header>
+          </Menu.Item>
+          <Menu.Menu position="right">
+            <Menu.Item icon onClick={() => this.handleSort('title')}>
+              <Icon
+                aria-label={`sort alphabet ${direction}ending`}
+                name={`sort alphabet ${direction}ending`}
+                size="large"
+                color={sortBy === 'title' ? 'teal' : 'black'}
+              />
+            </Menu.Item>
+            <Menu.Item icon onClick={() => this.handleSort('id')}>
+              <Icon
+                aria-label={`sort numeric ${direction}ending`}
+                name={`sort numeric ${direction}ending`}
+                size="large"
+                color={sortBy === 'id' ? 'teal' : 'black'}
+              />
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu>
 
         <Segment attached="bottom" className="content-segment">
           {/* Loader is active as long as both content and users are fetching */}
@@ -26,26 +66,33 @@ class ContentSegment extends React.Component {
 
           {!loading[contentType] &&
             !loading.users && (
-              <Card.Group centered>
-                {content.map(item => (
-                  <Card key={item.id}>
-                    <Card.Content>
-                      <Card.Header textAlign="left" content={item.title} />
-                      <Card.Meta
-                        textAlign="left"
-                        content={users[item.userId].name}
-                      />
-                      {/* if body exists, add it as card desciption */}
-                      {item.body && (
-                        <Card.Description
-                          textAlign="left"
-                          content={item.body}
-                        />
-                      )}
-                    </Card.Content>
-                  </Card>
-                ))}
-              </Card.Group>
+              <Datasort
+                data={content}
+                sortBy={sortBy}
+                direction={direction}
+                render={({ data }) => (
+                  <Card.Group centered>
+                    {data.map(item => (
+                      <Card key={item.id}>
+                        <Card.Content>
+                          <Card.Header textAlign="left" content={item.title} />
+                          <Card.Meta
+                            textAlign="left"
+                            content={users[item.userId].name}
+                          />
+                          {/* if body exists, add it as card desciption */}
+                          {item.body && (
+                            <Card.Description
+                              textAlign="left"
+                              content={item.body}
+                            />
+                          )}
+                        </Card.Content>
+                      </Card>
+                    ))}
+                  </Card.Group>
+                )}
+              />
             )}
         </Segment>
       </React.Fragment>
